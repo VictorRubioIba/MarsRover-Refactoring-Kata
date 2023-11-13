dd3d80ee2b7d8e8a88368ffe833502987032cf9f
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-13 17:45:54 +0100

    refactor: extract methods

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -6,49 +6,70 @@ public class MarsRover {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
             if (instruction == 'L') {
-                if (direction1.equals(Direction.N)) {
-                    return move(new Position(position.getX(), position.getY()), Direction.W,
-                                instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.W)) {
-                    return move(new Position(position.getX(), position.getY()),Direction.S,
-                                instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.S)) {
-                    return move(new Position(position.getX(), position.getY()), Direction.E,
-                                instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.E)) {
-                    return move(new Position(position.getX(), position.getY()), Direction.N,
-                                instructions.substring(1, instructions.length()));
-                }
+                String position1 = turnLeft(position, direction1, instructions);
+                if (position1 != null)
+                    return position1;
             } else if (instruction == 'R') {
-                if (direction1.equals(Direction.N)) {
-                    return move(new Position(position.getX(), position.getY()), Direction.E,
-                                instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.W)) {
-                    return move(new Position(position.getX(), position.getY()), Direction.N,
-                                instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.S)) {
-                    return move(new Position(position.getX(), position.getY()), Direction.W,
-                                instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.E)) {
-                    return move(new Position(position.getX(), position.getY()),Direction.S,
-                                instructions.substring(1, instructions.length()));
-                }
+                String position1 = turnRight(position, direction1, instructions);
+                if (position1 != null)
+                    return position1;
             } else if (instruction == 'M') {
-                if (direction1.equals(Direction.N)) {
-                    return move(new Position(position.getX(), position.getY() + 1),
-                                Direction.N, instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.S)) {
-                    return move(new Position(position.getX(), position.getY() - 1),
-                                Direction.S, instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.W)) {
-                    return move(new Position(position.getX() - 1, position.getY()),
-                                Direction.W, instructions.substring(1, instructions.length()));
-                } else if (direction1.equals(Direction.E)) {
-                    return move(new Position(position.getX() + 1, position.getY()),
-                                Direction.E, instructions.substring(1, instructions.length()));
-                }
+                String position1 = goFront(position, direction1, instructions);
+                if (position1 != null)
+                    return position1;
             }
         }
         return position.getX() + " " + position.getY() + " " + direction1;
+    }
+
+    private static String goFront(Position position, Direction direction1, String instructions) {
+        if (direction1.equals(Direction.N)) {
+            return move(new Position(position.getX(), position.getY() + 1),
+                        Direction.N, instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.S)) {
+            return move(new Position(position.getX(), position.getY() - 1),
+                        Direction.S, instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.W)) {
+            return move(new Position(position.getX() - 1, position.getY()),
+                        Direction.W, instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.E)) {
+            return move(new Position(position.getX() + 1, position.getY()),
+                        Direction.E, instructions.substring(1, instructions.length()));
+        }
+        return null;
+    }
+
+    private static String turnRight(Position position, Direction direction1, String instructions) {
+        if (direction1.equals(Direction.N)) {
+            return move(new Position(position.getX(), position.getY()), Direction.E,
+                        instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.W)) {
+            return move(new Position(position.getX(), position.getY()), Direction.N,
+                        instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.S)) {
+            return move(new Position(position.getX(), position.getY()), Direction.W,
+                        instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.E)) {
+            return move(new Position(position.getX(), position.getY()), Direction.S,
+                        instructions.substring(1, instructions.length()));
+        }
+        return null;
+    }
+
+    private static String turnLeft(Position position, Direction direction1, String instructions) {
+        if (direction1.equals(Direction.N)) {
+            return move(new Position(position.getX(), position.getY()), Direction.W,
+                        instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.W)) {
+            return move(new Position(position.getX(), position.getY()), Direction.S,
+                        instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.S)) {
+            return move(new Position(position.getX(), position.getY()), Direction.E,
+                        instructions.substring(1, instructions.length()));
+        } else if (direction1.equals(Direction.E)) {
+            return move(new Position(position.getX(), position.getY()), Direction.N,
+                        instructions.substring(1, instructions.length()));
+        }
+        return null;
     }
 }
