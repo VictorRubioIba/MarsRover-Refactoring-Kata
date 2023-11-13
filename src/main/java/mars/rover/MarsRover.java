5d310bdbbcf491c6075ffef9b760fa8047ce23b3
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-13 21:59:44 +0100

    refactor: rename parameters

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -2,34 +2,28 @@ package mars.rover;
 
 public class MarsRover {
 
-    public static String move(Position position, Direction direction1, String instructions) {
+    public static String move(Position position, Direction direction, String instructions) {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
 			switch (instruction) {
 				case 'L': {
-					if (turnLeft(position, direction1, instructions) != null)
-						return turnLeft(position, direction1, instructions);
-					break;
+					return turnLeft(position, direction, instructions);
 				}
 				case 'R': {
-					String position1 = turnRight(position, direction1, instructions);
-					if (position1 != null)
-						return position1;
-					break;
+					return turnRight(position, direction, instructions);
 				}
 				case 'M': {
-					String position1 = goFront(position, direction1, instructions);
-					if (position1 != null)
-						return position1;
-					break;
+					 return goFront(position, direction, instructions);
 				}
+				default:
+					throw new IllegalStateException("Unexpected value: " + instruction);
 			}
         }
-        return position.getX() + " " + position.getY() + " " + direction1;
+        return position.getX() + " " + position.getY() + " " + direction;
     }
 
-    private static String goFront(Position position, Direction direction1, String instructions) {
-		switch (direction1) {
+    private static String goFront(Position position, Direction direction, String instructions) {
+		switch (direction) {
 			case N:
 				return move(new Position(position.getX(), position.getY() + 1),
 							Direction.N, instructions.substring(1, instructions.length()));
@@ -43,13 +37,13 @@ public class MarsRover {
 				return move(new Position(position.getX() + 1, position.getY()),
 							Direction.E, instructions.substring(1, instructions.length()));
 			default:
-				throw new IllegalStateException("Unexpected value: " + direction1);
+				throw new IllegalStateException("Unexpected value: " + direction);
 		}
 
     }
 
-    private static String turnRight(Position position, Direction direction1, String instructions) {
-		switch (direction1) {
+    private static String turnRight(Position position, Direction direction, String instructions) {
+		switch (direction) {
 			case N:
 				return move(new Position(position.getX(), position.getY()), Direction.E,
 							instructions.substring(1, instructions.length()));
@@ -63,7 +57,7 @@ public class MarsRover {
 				return move(new Position(position.getX(), position.getY()), Direction.S,
 							instructions.substring(1, instructions.length()));
 			default:
-				throw new IllegalStateException("Unexpected value: " + direction1);
+				throw new IllegalStateException("Unexpected value: " + direction);
 		}
 
     }
