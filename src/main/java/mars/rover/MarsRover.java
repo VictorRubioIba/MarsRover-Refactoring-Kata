b3917ca192e9b905d725962f843d77f0b7dacb3c
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-16 18:02:30 +0100

    refactor: create new parameter object position

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -2,41 +2,41 @@ package mars.rover;
 
 public class MarsRover {
 
-    public static String move(int x, int y, char direction, String instructions) {
+    public static String move(Position position, char direction, String instructions) {
         if (!instructions.isEmpty()) {
             char instruction = instructions.charAt(0);
             if (instruction == 'L') {
                 if (direction == 'N') {
-                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'S', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'E', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'N', instructions.substring(1, instructions.length()));
                 }
             } else if (instruction == 'R') {
                 if (direction == 'N') {
-                    return move(x, y, 'E', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'E', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x, y, 'N', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'N', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y, 'W', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x, y, 'S', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY()), 'S', instructions.substring(1, instructions.length()));
                 }
             } else if (instruction == 'M') {
                 if (direction == 'N') {
-                    return move(x, y + 1, 'N', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY() + 1), 'N', instructions.substring(1, instructions.length()));
                 } else if (direction == 'S') {
-                    return move(x, y - 1, 'S', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX(), position.getY() - 1), 'S', instructions.substring(1, instructions.length()));
                 } else if (direction == 'W') {
-                    return move(x - 1, y, 'W', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX() - 1, position.getY()), 'W', instructions.substring(1, instructions.length()));
                 } else if (direction == 'E') {
-                    return move(x + 1, y, 'E', instructions.substring(1, instructions.length()));
+                    return move(new Position(position.getX() + 1, position.getY()), 'E', instructions.substring(1, instructions.length()));
                 }
             }
         }
-        return x + " " + y + " " + direction;
+        return position.getX() + " " + position.getY() + " " + direction;
     }
 }
