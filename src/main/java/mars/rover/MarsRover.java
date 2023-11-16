fb8705b5829ff1f40f055b5b4a8e0a429ecb67d1
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-16 18:28:26 +0100

    refactor: extract method gofront

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -11,36 +11,52 @@ public class MarsRover {
                     return position1;
                 }
             } else if (instruction == 'R') {
-                if (direction1.equals(Direction.N)) {
-                    return move(goNewPosition(position), Direction.E,
-                                getInstructions(instructions));
-                } else if (direction1.equals(Direction.W)) {
-                    return move(goNewPosition(position), Direction.N,
-                                getInstructions(instructions));
-                } else if (direction1.equals(Direction.S)) {
-                    return move(goNewPosition(position), Direction.W,
-                                getInstructions(instructions));
-                } else if (direction1.equals(Direction.E)) {
-                    return move(goNewPosition(position), Direction.S,
-                                getInstructions(instructions));
+                String position1 = turnRight(position, direction1, instructions);
+                if (position1 != null) {
+                    return position1;
                 }
             } else if (instruction == 'M') {
-                if (direction1.equals(Direction.N)) {
-                    return move(new Position(position.getX(), position.getY() + 1),
-                                Direction.N, getInstructions(instructions));
-                } else if (direction1.equals(Direction.S)) {
-                    return move(new Position(position.getX(), position.getY() - 1),
-                                Direction.S, getInstructions(instructions));
-                } else if (direction1.equals(Direction.W)) {
-                    return move(new Position(position.getX() - 1, position.getY()),
-                                Direction.W, getInstructions(instructions));
-                } else if (direction1.equals(Direction.E)) {
-                    return move(new Position(position.getX() + 1, position.getY()),
-                                Direction.E, getInstructions(instructions));
+                String position1 = goFront(position, direction1, instructions);
+                if (position1 != null) {
+                    return position1;
                 }
             }
         }
         return position.getX() + " " + position.getY() + " " + direction1;
+    }
+
+    private static String goFront(Position position, Direction direction1, String instructions) {
+        if (direction1.equals(Direction.N)) {
+            return move(new Position(position.getX(), position.getY() + 1),
+                        Direction.N, getInstructions(instructions));
+        } else if (direction1.equals(Direction.S)) {
+            return move(new Position(position.getX(), position.getY() - 1),
+                        Direction.S, getInstructions(instructions));
+        } else if (direction1.equals(Direction.W)) {
+            return move(new Position(position.getX() - 1, position.getY()),
+                        Direction.W, getInstructions(instructions));
+        } else if (direction1.equals(Direction.E)) {
+            return move(new Position(position.getX() + 1, position.getY()),
+                        Direction.E, getInstructions(instructions));
+        }
+        return null;
+    }
+
+    private static String turnRight(Position position, Direction direction1, String instructions) {
+        if (direction1.equals(Direction.N)) {
+            return move(goNewPosition(position), Direction.E,
+                        getInstructions(instructions));
+        } else if (direction1.equals(Direction.W)) {
+            return move(goNewPosition(position), Direction.N,
+                        getInstructions(instructions));
+        } else if (direction1.equals(Direction.S)) {
+            return move(goNewPosition(position), Direction.W,
+                        getInstructions(instructions));
+        } else if (direction1.equals(Direction.E)) {
+            return move(goNewPosition(position), Direction.S,
+                        getInstructions(instructions));
+        }
+        return null;
     }
 
     private static String turnLeft(Position position, Direction direction1, String instructions) {
