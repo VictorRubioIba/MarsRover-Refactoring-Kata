bffb3e2163a7c021273366d1f02710ac93ba48bd
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-16 18:39:40 +0100

    refactor: refactor change switch

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -2,89 +2,83 @@ package mars.rover;
 
 public class MarsRover {
 
-    public static String move(Position position, Direction direction1, String instructions) {
-        if (!instructions.isEmpty()) {
-            char instruction = instructions.charAt(0);
-            if (instruction == 'L') {
-                String position1 = turnLeft(position, direction1, instructions);
-                if (position1 != null) {
-                    return position1;
-                }
-            } else if (instruction == 'R') {
-                String position1 = turnRight(position, direction1, instructions);
-                if (position1 != null) {
-                    return position1;
-                }
-            } else if (instruction == 'M') {
-                String position1 = goFront(position, direction1, instructions);
-                if (position1 != null) {
-                    return position1;
-                }
-            }
+  public static String move(Position position, Direction direction1, String instructions) {
+    if (!instructions.isEmpty()) {
+      char instruction = instructions.charAt(0);
+      if (instruction == 'L') {
+        String position1 = turnLeft(position, direction1, instructions);
+        if (position1 != null) {
+          return position1;
         }
-        return position.getX() + " " + position.getY() + " " + direction1;
+      } else if (instruction == 'R') {
+        String position1 = turnRight(position, direction1, instructions);
+        if (position1 != null) {
+          return position1;
+        }
+      } else if (instruction == 'M') {
+        String position1 = goFront(position, direction1, instructions);
+        if (position1 != null) {
+          return position1;
+        }
+      }
     }
+    return position.getX() + " " + position.getY() + " " + direction1;
+  }
 
-    private static String goFront(Position position, Direction direction1, String instructions) {
-		switch (direction1) {
-			case N:
-				return move(new Position(position.getX(), position.getY() + 1),
-							Direction.N, getInstructions(instructions));
-			case S:
-				return move(new Position(position.getX(), position.getY() - 1),
-							Direction.S, getInstructions(instructions));
-			case W:
-				return move(new Position(position.getX() - 1, position.getY()),
-							Direction.W, getInstructions(instructions));
-			case E:
-				return move(new Position(position.getX() + 1, position.getY()),
-							Direction.E, getInstructions(instructions));
-		}
-        return null;
+  private static String goFront(Position position, Direction direction1, String instructions) {
+    switch (direction1) {
+      case N:
+        return move(
+            new Position(position.getX(), position.getY() + 1),
+            Direction.N,
+            getInstructions(instructions));
+      case S:
+        return move(
+            new Position(position.getX(), position.getY() - 1),
+            Direction.S,
+            getInstructions(instructions));
+      case W:
+        return move(
+            new Position(position.getX() - 1, position.getY()),
+            Direction.W,
+            getInstructions(instructions));
+      case E:
+        return move(
+            new Position(position.getX() + 1, position.getY()),
+            Direction.E,
+            getInstructions(instructions));
     }
+    return null;
+  }
 
-    private static String turnRight(Position position, Direction direction1, String instructions) {
-		switch (direction1) {
-			case N:
-				return move(goNewPosition(position), Direction.E,
-							getInstructions(instructions));
-			case W:
-				return move(goNewPosition(position), Direction.N,
-							getInstructions(instructions));
-			case S:
-				return move(goNewPosition(position), Direction.W,
-							getInstructions(instructions));
-			case E:
-				return move(goNewPosition(position), Direction.S,
-							getInstructions(instructions));
-		}
-        return null;
+  private static String turnRight(Position position, Direction direction1, String instructions) {
+    switch (direction1) {
+      case N:
+        return move(goNewPosition(position), Direction.E, getInstructions(instructions));
+      case W:
+        return move(goNewPosition(position), Direction.N, getInstructions(instructions));
+      case S:
+        return move(goNewPosition(position), Direction.W, getInstructions(instructions));
+      case E:
+        return move(goNewPosition(position), Direction.S, getInstructions(instructions));
     }
+    return null;
+  }
 
-    private static String turnLeft(Position position, Direction direction1, String instructions) {
-		switch (direction1) {
-			case N:
+  private static String turnLeft(Position position, Direction direction1, String instructions) {
+    return switch (direction1) {
+      case N -> move(goNewPosition(position), Direction.W, getInstructions(instructions));
+      case W -> move(goNewPosition(position), Direction.S, getInstructions(instructions));
+      case S -> move(goNewPosition(position), Direction.E, getInstructions(instructions));
+      default -> move(goNewPosition(position), Direction.N, getInstructions(instructions));
+    };
+  }
 
-				return move(goNewPosition(position), Direction.W, getInstructions(instructions));
+  private static Position goNewPosition(Position position) {
+    return new Position(position.getX(), position.getY());
+  }
 
-			case W:
-				return move(goNewPosition(position), Direction.S,
-							getInstructions(instructions));
-			case S:
-				return move(goNewPosition(position), Direction.E,
-							getInstructions(instructions));
-			case E:
-				return move(goNewPosition(position), Direction.N,
-							getInstructions(instructions));
-		}
-        return null;
-    }
-
-    private static Position goNewPosition(Position position) {
-        return new Position(position.getX(), position.getY());
-    }
-
-    private static String getInstructions(String instructions) {
-        return instructions.substring(1, instructions.length());
-    }
+  private static String getInstructions(String instructions) {
+    return instructions.substring(1, instructions.length());
+  }
 }
