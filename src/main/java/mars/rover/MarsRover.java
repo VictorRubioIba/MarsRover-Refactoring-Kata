785e9a901f66789caee7ef68238df8b84e47bf97
Victor <92888336+IbanezRubioVictor@users.noreply.github.com>
2023-11-16 18:11:15 +0100

    refactor: create new method gonewposition

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -7,23 +7,23 @@ public class MarsRover {
             char instruction = instructions.charAt(0);
             if (instruction == 'L') {
                 if (direction == 'N') {
-                    return move(new Position(position.getX(), position.getY()), 'W', getInstructions(instructions));
+                    return move(goNewPosition(position), 'W', getInstructions(instructions));
                 } else if (direction == 'W') {
-                    return move(new Position(position.getX(), position.getY()), 'S', getInstructions(instructions));
+                    return move(goNewPosition(position), 'S', getInstructions(instructions));
                 } else if (direction == 'S') {
-                    return move(new Position(position.getX(), position.getY()), 'E', getInstructions(instructions));
+                    return move(goNewPosition(position), 'E', getInstructions(instructions));
                 } else if (direction == 'E') {
-                    return move(new Position(position.getX(), position.getY()), 'N', getInstructions(instructions));
+                    return move(goNewPosition(position), 'N', getInstructions(instructions));
                 }
             } else if (instruction == 'R') {
                 if (direction == 'N') {
-                    return move(new Position(position.getX(), position.getY()), 'E', getInstructions(instructions));
+                    return move(goNewPosition(position), 'E', getInstructions(instructions));
                 } else if (direction == 'W') {
-                    return move(new Position(position.getX(), position.getY()), 'N', getInstructions(instructions));
+                    return move(goNewPosition(position), 'N', getInstructions(instructions));
                 } else if (direction == 'S') {
-                    return move(new Position(position.getX(), position.getY()), 'W', getInstructions(instructions));
+                    return move(goNewPosition(position), 'W', getInstructions(instructions));
                 } else if (direction == 'E') {
-                    return move(new Position(position.getX(), position.getY()), 'S', getInstructions(instructions));
+                    return move(goNewPosition(position), 'S', getInstructions(instructions));
                 }
             } else if (instruction == 'M') {
                 if (direction == 'N') {
@@ -38,6 +38,10 @@ public class MarsRover {
             }
         }
         return position.getX() + " " + position.getY() + " " + direction;
+    }
+
+    private static Position goNewPosition(Position position) {
+        return new Position(position.getX(), position.getY());
     }
 
     private static String getInstructions(String instructions) {
